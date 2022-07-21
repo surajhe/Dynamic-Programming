3e1986ad64352a413795d9e32c09b5d1b5662c9c
surajhe <86972874+surajhe@users.noreply.github.com>
2022-07-21 14:14:28 +0530

    Create MaxSubsequenceSum.cpp

---
+++ MaxSubsequenceSum.cpp
@@ -0,0 +1,20 @@
+// Given an aaray find max. subsequence sum
+// In a subsequence, 2 adjacent elements can not be picked.
+
+int maxSubsequenceSum(vector<int> V)
+{
+  int n = V.size();
+  
+  int dp[n+1];
+  
+  dp[0] = 0;
+  dp[1] = max(V[0], V[1]);
+  
+  for(int i=0; i<=n; i++)
+  {
+    dp[i] = max(dp[i-1], dp[i-2] + V[i]);
+  }
+  
+  return dp[n-1];
+  
+}
